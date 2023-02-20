0f85a99f6d01b197bb0e53a8e08533adbc11be36
amanialosaimi <amanialossaimi@gmail.com>
2023-02-20 21:22:38 +0300

    Initial Commit

---
+++ counter/counterApp.swift
@@ -0,0 +1,17 @@
+//
+//  counterApp.swift
+//  counter
+//
+//  Created by Amani Al Osaimi on 29/07/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct counterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
